ee91ce884f198167c1a46ff0ffd194e09763864a
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2024-10-08 12:55:45 -0400

    Update __init__.py

--- h2w_aztools/__init__.py
+++ h2w_aztools/__init__.py
@@ -1,3 +1,6 @@
+from h2w_aztools.azmonitor import *
+from h2w_aztools.loganalytics import *
 from h2w_aztools.m365d import *
 from h2w_aztools.oath import *
-from h2w_aztools.oath import *
+from h2w_aztools.sentinel import *
+
