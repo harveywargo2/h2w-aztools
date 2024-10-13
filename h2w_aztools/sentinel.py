13b93eb87d6b10ff66d3f137d32703dbf0bbc880
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2024-10-13 09:14:22 -0400

    Update sentinel.py

--- h2w_aztools/sentinel.py
+++ h2w_aztools/sentinel.py
@@ -2,7 +2,28 @@ from h2w_aztools.azmonitor import AzMonitorRunQuery
 
 
 class SentinelRunQuery(AzMonitorRunQuery):
+    """A class to store and instance of data from an API call to run a query against sentinel data
+    class inherited from AzMonitorRunQuery
+    Sentinel sits on top of Azure Monitor
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    ```
+    attributes
+    ----------
+        query_json (dict) : api query formatting
+        request_url (str) : hardcoded request api url
+        request_headers (dict) : hardcoded request headers
+        response_json (dict) : response from api call
+        pull_date (datetime) : timestamp of api call
+        response_df (dataframe) : convert to pandas dataframe object
+    """
+
+    def __init__(self, oath_token, query_text, workspace_id):
+        """
+        sets attributes for instance of object
+
+        :param oath_token: (str) graph api bearer token
+        :param query_text: (str) advanced hunting query text
+        :param workspace_id: (str) azure monitor workspace id
+        """
+        super().__init__(oath_token, query_text, workspace_id)
 
