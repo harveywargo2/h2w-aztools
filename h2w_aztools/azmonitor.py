ca12cacb2beaa3ceb6367233e5b840848c054e47
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2024-10-13 08:52:02 -0400

    Update azmonitor.py

--- h2w_aztools/azmonitor.py
+++ h2w_aztools/azmonitor.py
@@ -3,11 +3,35 @@ import pandas as pd
 
 
 class AzMonitorRunQuery:
+    """A class to store and instance of data from an API call to run a query against azure monitor data
 
-    def __init__(self, **kwargs):
-        self.oath_token = kwargs.get('oath_token', 'error')
-        self.query_text = kwargs.get('query_text', 'error')
-        self.workspace_id = kwargs.get('workspace_id', 'error')
+    ```
+    attributes
+    ----------
+        query_json (dict) : api query formatting
+        request_url (str) : hardcoded request api url
+        request_headers (dict) : hardcoded request headers
+        response_json (dict) : response from api call
+        pull_date (datetime) : timestamp of api call
+        response_df (dataframe) : convert to pandas dataframe object
+    """
+
+    def __init__(self, oath_token='null', query_text='null', workspace_id='null'):
+        """
+        sets attributes for instance of object
+
+        :param oath_token: (str) graph api bearer token
+        :param query_text: (str) advanced hunting query text
+        :param workspace_id: (str) azure monitor workspace id
+        """
+
+        # parameters
+        self.oath_token = oath_token
+        self.query_text = query_text
+        self.workspace_id = workspace_id
+
+        # attributes
+        self.query_json = {'query': self.query_text}
         self.request_url = f'https://api.loganalytics.azure.com/v1/workspaces/{self.workspace_id}/query'
         self.request_headers = {
             'Authorization': f'Bearer {self.oath_token}',
@@ -17,10 +41,15 @@ class AzMonitorRunQuery:
         self.response_json = requests.request('POST',
                                               self.request_url,
                                               headers=self.request_headers,
-                                              json=self.query_text).json()
+                                              json=self.query_json).json()
         self.response_df = self._to_df()
 
     def _to_df(self):
+        """
+        converts dict/json to pandas dataframe
+
+        :return: dataframe stored in attribute
+        """
         col_names_df = []
 
         if 'tables' in self.response_json:
