8858e5361a29993e4879fadcdab056a6ff8f3550
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2025-01-29 07:15:05 -0500

    Update sentinel.py

--- aztools/sentinel.py
+++ aztools/sentinel.py
@@ -1,28 +1,36 @@
 from aztools.azmonitor import AzMonitorRunQuery
+import requests
+import datetime
 
 
 class SentinelRunQuery(AzMonitorRunQuery):
-    """A class to store and instance of data from an API call to run a query against sentinel data
-    class inherited from AzMonitorRunQuery
-    Sentinel sits on top of Azure Monitor
 
-    ```
-    attributes
-    ----------
-        query_json (dict) : api query formatting
-        request_url (str) : hardcoded request api url
-        request_headers (dict) : hardcoded request headers
-        response_json (dict) : response from api call
-        pull_date (datetime) : timestamp of api call
-        response_df (dataframe) : convert to pandas dataframe object
-    """
 
     def __init__(self, oauth_token, query_text, workspace_id):
-        """
-        sets attributes for instance of object
-
-        :param oauth_token: (str) graph api bearer token
-        :param query_text: (str) advanced hunting query text
-        :param workspace_id: (str) azure monitor workspace id
-        """
         super().__init__(oauth_token, query_text, workspace_id)
+
+
+class SentinelListRules:
+
+
+    def __init__(self, subscription_id, resource_group, workspace, auth_token, api_version='2024-09-01'):
+
+        # parameter
+        self.subscription_id = subscription_id
+        self.resource_group = resource_group
+        self.workspace = workspace
+        self.api_version = api_version
+        self.auth_token = auth_token
+
+        # attributes
+        self.request_url = f'https://management.azure.com/subscriptions/{self.subscription_id}/resourceGroups/{self.resource_group}/providers/Microsoft.OperationalInsights/workspaces/{self.workspace}/providers/Microsoft.SecurityInsights/alertRules?api-version={self.api_version}'
+        self.request_headers = {
+            'Authorization': f'Bearer {self.auth_token}',
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+        self.response_json = requests.request('GET',
+                                              self.request_url,
+                                              headers=self.request_headers).json()
+        self.pull_date = datetime.datetime.now()
+
