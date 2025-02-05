07a67905c1154cac0173d9b9477f9c6de1a89ab3
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2025-02-05 11:37:21 -0500

    Update sentinel.py

--- aztools/sentinel.py
+++ aztools/sentinel.py
@@ -8,21 +8,21 @@ import pandas as pd
 class SentinelRunQuery(AzMonitorRunQuery):
 
 
-    def __init__(self, oauth_token, query_text, workspace_id):
-        super().__init__(oauth_token, query_text, workspace_id)
+    def __init__(self, la_oauth_token, query_text, workspace_id):
+        super().__init__(la_oauth_token, query_text, workspace_id)
 
 
 class SentinelListRules:
 
 
-    def __init__(self, subscription_id, resource_group, workspace, auth_token, api_version='2024-09-01'):
+    def __init__(self, subscription_id, resource_group, workspace, arm_auth_token, api_version='2024-09-01'):
 
         # parameter
         self.subscription_id = subscription_id
         self.resource_group = resource_group
         self.workspace = workspace
         self.api_version = api_version
-        self.auth_token = auth_token
+        self.auth_token = arm_auth_token
 
         # attributes
         self.request_url = f'https://management.azure.com/subscriptions/{self.subscription_id}/resourceGroups/{self.resource_group}/providers/Microsoft.OperationalInsights/workspaces/{self.workspace}/providers/Microsoft.SecurityInsights/alertRules?api-version={self.api_version}'
